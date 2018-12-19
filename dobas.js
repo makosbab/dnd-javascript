02ab3453b57d23a3f9ca5727cf47ea44355413a2
Róbert Tóth <robert.toth.ext@nokia.com>
2018-12-19 16:24:48 +0100

    HTML

--- dobas.js
+++ dobas.js
@@ -5,9 +5,11 @@ var Dobas = function (db, oldalak, szorzo, bonusz) {
     this.oldalak = oldalak;
     this.szorzo = szorzo;
     this.bonusz = parseInt(bonusz);
+
 }
 
-var eleteroDobas = function(dobas){
+
+var eleteroDobas = function(eleteroDobas){
     this.dobas = new Dobas()
     this.eletPont = 0
 }
@@ -35,6 +37,21 @@ var dobj_ujra = dobas => {
     return eredmeny;
 }
 
+
+var Tulajdonsag = function (rovidNev, ertek) {
+    
+    this.rovidNev = rovidNev
+    this.ertek = ertek
+    // this.tulModosito = Math.floor((this.ertek - 10 ) / 2)
+    this.tulModosito = function() {return Math.floor((this.ertek - 10) / 2)}
+   
+}
+
 var found = regexDobas.exec("8d6+45");
 dobas = new Dobas(found[1], found[2], found[3], found[4]);
 console.log(dobj_ujra(dobas));
+var tulajdonsag = new Tulajdonsag("Áll", 19)
+console.log(tulajdonsag.tulModosito())
+tulajdonsag.ertek = 28
+console.log(tulajdonsag.ertek)
+console.log(tulajdonsag.tulModosito())
